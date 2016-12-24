ff104b349a73dc965b1ee2b188fe760a855c116e
Vuk Dukic <vukdukic@gmail.com>
2016-12-24 09:27:45 -0800

    added changes

--- ComponentNestingExample.js
+++ ComponentNestingExample.js
@@ -5,3 +5,18 @@ var Badge = React.createElement({
     </button>
   }
 });
+
+var Form = React.createClass({
+  render: function() {
+    return <div>
+      <Badge title={this.props.title} number={this.props.number} />
+    </div>
+  }
+});
+
+// Create Form
+var options = {
+  title: 'Inbox'
+  number: 5
+}
+React.createElement(Form, options)
